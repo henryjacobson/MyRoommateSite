93e95746a48b7b13a26fa1c0116a3c361565ef9c
Henry Jacobson <henryajacobson@gmail.com>
2020-12-13 22:43:30 -0600

    Update facilityReducer.js

--- src/reducers/facilityReducer.js
+++ src/reducers/facilityReducer.js
@@ -3,15 +3,15 @@ import {
     GET_ALL_FACILITIES,
     DELETE_FACILITY,
     CREATE_FACILITY,
-    UPDATE_FACILITY, LOCAL_UPDATE_FACILITY
+    UPDATE_FACILITY
 } from "../actions/facilityActions"
 
 const initialState = {
     facilities: [{ id: 1, title: "facility1" }, { id: 2, title: "facility2" }, { id: 3, title: "facility" }],
     facility: {
-        title: '',
-        description: '',
-        status: ''
+        type: '',
+        status: '',
+        notes: ''
     }
 }
 
